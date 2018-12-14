1fef5c087149d371c411b0474f8af4f094ad0633
AssassinG <hangq_zju@hotmail.com>
2018-12-14 15:34:00 +0800

    init commit

---
+++ src/main/java/cn/AssassinG/ScsyERP/OutStorage/facade/enums/OutStorageFormStatus.java
@@ -0,0 +1,52 @@
+package cn.AssassinG.ScsyERP.OutStorage.facade.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum OutStorageFormStatus {
+    Workging("正在出库", 0), Done("出库完成", 1);
+    private String Name;
+    private Integer Value;
+
+    static Map<String, OutStorageFormStatus> enumMap = new HashMap<String, OutStorageFormStatus>();
+    static{
+        for(OutStorageFormStatus type : OutStorageFormStatus.values()){
+            enumMap.put(type.getName(), type);
+        }
+    }
+
+    private OutStorageFormStatus(String name, Integer value) {
+        Name = name;
+        Value = value;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Integer getValue() {
+        return Value;
+    }
+
+    public void setValue(Integer value) {
+        Value = value;
+    }
+
+    public static OutStorageFormStatus getEnum(String name){
+        return enumMap.get(name);
+    }
+
+    static Map<Integer, OutStorageFormStatus> intMap = new HashMap<>();
+    static{
+        for(OutStorageFormStatus type : OutStorageFormStatus.values()){
+            intMap.put(type.getValue(), type);
+        }
+    }
+    public static OutStorageFormStatus getEnum(Integer value){
+        return intMap.get(value);
+    }
+}
